53bf0e05abd073332b48276c8d498ceff56eecf2
alzah <alzahfariski@gmail.com>
2022-12-09 10:05:05 +0700

    tambah model pengaduan

--- petugas/models.py
+++ petugas/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.utils.text import slugify
+from auntentifikasi.models import UserProfileInfo
 
 # Create your models here.
 class artikel(models.Model):
@@ -28,6 +29,21 @@ class kategori(models.Model):
     sifat = models.CharField(max_length=100)
     jenis = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.nama
+
+class pengaduan(models.Model):
+    nama = models.CharField(max_length=100)
+    jalan = models.CharField(max_length=200)
+    kecamatan = models.CharField(max_length=100)
+    keterangan = models.CharField(max_length=300)
+    foto  = models.ImageField(upload_to='pengaduan/')
+    kategori_id = models.ForeignKey(kategori,  on_delete=models.CASCADE)
+    pengadu_id = models.ForeignKey(UserProfileInfo, on_delete=models.CASCADE)
+    
     class Meta:
         ordering = ['-id']
 

695dd261d10627c342c64a440871af5394f6ca24
alzah <alzahfariski@gmail.com>
2022-12-11 15:37:00 +0700

    menampilkan data di awal

--- simpegal/view.py
+++ simpegal/view.py
@@ -11,6 +11,9 @@ def index(request):
         'page_title': 'simpegal',
         'berita':berita,
         'kategori' : kategori,
+        'total_pengaduan':len(models.aduan.objects.all()),
+        'total_konfirmasi':len(models.aduan.objects.filter(status = 'konfirmasi')),
+        'total_proses':len(models.aduan.objects.filter(status = 'proses')),
     }
     return render(request, 'index.html',context)
 
